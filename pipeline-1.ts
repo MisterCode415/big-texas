5008dc11e33cf9011b9a5a79dd816f82cca21f86
PapaViking <brianvegan@protonmail.com>
2024-11-21 16:11:12 -0800

    runner setup

--- pipeline-1.ts
+++ pipeline-1.ts
@@ -13,6 +13,11 @@ import fs from 'fs';
 import { MongoClient } from 'mongodb';
 dotenv.config();
 
+// get arguments
+const args = process.argv;
+const batchSize = args.find(arg => arg.startsWith('limit='))?.split('=')[1] || '10';
+const offset = args.find(arg => arg.startsWith('offset='))?.split('=')[1] || '0';
+
 //use your `key` and `endpoint` environment variables
 const apiKey = process.env['DI_KEY'];
 const endpoint = process.env['DI_ENDPOINT'];
@@ -32,55 +37,65 @@ async function streamToBuffer(readableStream: NodeJS.ReadableStream | undefined)
     });
 }
 
-async function main() {
+async function main(options: { batchSize: string, offset: string }) {
     console.log('starting...');
+    const mongoClient = new MongoClient(process.env.MONGODB_URI);
+    await mongoClient.connect();
+    const db = mongoClient.db('big-texas');
     const AZURE_STORAGE_CONNECTION_STRING: string = process.env.AZURE_STORAGE_CONNECTION_STRING;
     if (!AZURE_STORAGE_CONNECTION_STRING) {
         throw new Error('AZURE_STORAGE_CONNECTION_STRING is not set');
     }
-    const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_STORAGE_CONNECTION_STRING);
-    const containerClient = blobServiceClient.getContainerClient('us-leases');
-    const documentId = '100042419';
-    let blockBlobClient = containerClient.getBlobClient("texas/reeves/1/100042419/DOC-100042419.pdf");
-    const pdfDataStream = await blockBlobClient.download();
-    const pdfData = await streamToBuffer(pdfDataStream.readableStreamBody);
-    const client = DocumentIntelligence(endpoint, new AzureKeyCredential(apiKey));
-    const initialResponse = await client
-        .path("/documentModels/{modelId}:analyze", "prebuilt-layout")
-        .post({
-            contentType: "application/json",
-            body: {
-                base64Source: pdfData.toString('base64'),
-            },
-            headers: {
-                "x-ms-lease-id": "c30d315d-95db-4fc5-b2b7-5a330265702f"
-            },
-            queryParameters: { outputContentFormat: "text" },
-        });
+    const cursor = db
+        .collection('leases-meta')
+        .find({ text: { $eq: null } })
+        .skip(parseInt(options.offset))
+        .limit(parseInt(options.batchSize));
+    for await (const lease of cursor) {
+        const internalId = lease.internalId;
+        const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_STORAGE_CONNECTION_STRING);
+        const containerClient = blobServiceClient.getContainerClient('us-leases');
+        let blockBlobClient = containerClient.getBlockBlobClient("texas/reeves/1/" + internalId + "/DOC-" + internalId + ".pdf");
+        const pdfDataStream = await blockBlobClient.download();
+        const pdfData = await streamToBuffer(pdfDataStream.readableStreamBody);
+        console.log('AI analysis in progress: ', internalId);
+        const client = DocumentIntelligence(endpoint, new AzureKeyCredential(apiKey));
+        const initialResponse = await client
+            .path("/documentModels/{modelId}:analyze", "prebuilt-layout")
+            .post({
+                contentType: "application/json",
+                body: {
+                    base64Source: pdfData.toString('base64'),
+                },
+                headers: {
+                    "x-ms-lease-id": "c30d315d-95db-4fc5-b2b7-5a330265702f"
+                },
+                queryParameters: { outputContentFormat: "text" },
+            });
 
 
-    // save to disk
-    if (isUnexpected(initialResponse)) {
-        throw initialResponse.body.error;
-    }
-    console.log('initialized, here is the transaction id: ', initialResponse);
-    const results: any = await pollUntilDone(client, initialResponse as unknown as AnalyzeResultOperationOutput);
-    console.log('result: ', results);
+        // save to disk
+        if (isUnexpected(initialResponse)) {
+            throw initialResponse.body.error;
+        }
+        console.log('initialized, here is the transaction id: ', initialResponse);
+        const results: any = await pollUntilDone(client, initialResponse as unknown as AnalyzeResultOperationOutput);
+        console.log('result: ', results);
 
-    // save to blob
-    blockBlobClient = containerClient.getBlockBlobClient('texas/reeves/1/' + documentId + '/' + documentId + '-analysis.json');
-    await blockBlobClient.upload(JSON.stringify(results, null, 2), Buffer.byteLength(JSON.stringify(results, null, 2)));
-    // save text only, to store
-    const mongoClient = new MongoClient(process.env.MONGODB_URI);
-    await mongoClient.connect();
-    const db = mongoClient.db('big-texas');
-    const collection = db.collection('leases-meta');
-    try {
-        const textResult = results[0].result.body.analyzeResult.content;
-        const updateResult = await collection.updateOne({ internalId: documentId }, { $set: { text: textResult } }, { upsert: false });
-        console.log('updateResult: ', updateResult);
-    } catch (error) {
-        console.error('error, skipping mongo update: ', error);
+        // save to blob
+        blockBlobClient = containerClient.getBlockBlobClient('texas/reeves/1/' + internalId + '/' + internalId + '-analysis.json');
+        await blockBlobClient.upload(JSON.stringify(results, null, 2), Buffer.byteLength(JSON.stringify(results, null, 2)));
+        // save text only, to store
+
+        const collection = db.collection('leases-meta');
+        try {
+            const textResult = results[0].result.body.analyzeResult.content;
+            const updateResult = await collection.updateOne({ internalId: internalId }, { $set: { text: textResult } }, { upsert: false });
+            console.log('updateResult: ', updateResult);
+        } catch (error) {
+            console.error('error, skipping mongo update: ', error);
+        }
+        console.log('finished: ', internalId);
     }
     await mongoClient.close();
 }
@@ -99,7 +114,10 @@ async function pollUntilDone(client, initialResponse: AnalyzeResultOperationOutp
 }
 
 
-main().catch((error) => {
+main({
+    batchSize: batchSize,
+    offset: offset
+}).catch((error) => {
     console.error("An error occurred:", error);
     process.exit(1);
 });
